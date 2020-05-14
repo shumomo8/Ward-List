dbec5b4f4f6feed015ca9bb5a7460cfa88c516f8
shu fujita <momo.8jack@gmail.com>
2020-05-14 19:06:34 +0900

    Initial Commit

---
+++ Ward List/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Ward List
+//
+//  Created by Shu Fujita on 2020/05/14.
+//  Copyright © 2020 Fujita shu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
