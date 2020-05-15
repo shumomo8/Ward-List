bdf62c1e31adeff7e2449d9cf1b64048c2076e63
shu fujita <momo.8jack@gmail.com>
2020-05-15 23:13:43 +0900

    WordList完成

--- Ward List/ViewController.swift
+++ Ward List/ViewController.swift
@@ -15,6 +15,30 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func back (sender: UIStoryboardSegue){
+    }
+    
+    @IBAction func startButtonTapped(){
+        let saveDate = UserDefaults.standard
+        if saveDate.array(forKey: "WORD") != nil{
+            if saveDate.array(forKey: "WORD")!.count > 0 {
+                performSegue(withIdentifier: "toQuestionView", sender: nil)
+            }
+        }else{
+            let alert = UIAlertController(
+            title: "単語",
+            message: "まずは「単語一覧」をタップして単語登録してください",
+            preferredStyle: .alert
+            )
+            alert.addAction(UIAlertAction(
+            title: "OK",
+            style: .default,
+            handler: nil
+            ))
+            
+            self.present(alert, animated: true, completion: nil)
+        }
+    }
+    
 }
 
